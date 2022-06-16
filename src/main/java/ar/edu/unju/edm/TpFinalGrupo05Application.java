6c79c2887c6652b239f82234bc31c0ff83c58f08
Leonel19Hub <leonel19coraite@gmail.com>
2022-06-16 18:06:47 -0300

    Inicializacion del Tp Final

---
+++ src/main/java/ar/edu/unju/edm/TpFinalGrupo05Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalGrupo05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalGrupo05Application.class, args);
+	}
+
+}
