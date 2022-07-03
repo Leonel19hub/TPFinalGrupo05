a974f42edf1bd36da5faad8389161ac374ab2c27
Nachox <Nachox@DESKTOP-NP450AC>
2022-07-03 19:17:23 -0300

    UsuarioService

--- src/main/java/ar/edu/unju/edm/service/IUsuarioService.java
+++ src/main/java/ar/edu/unju/edm/service/IUsuarioService.java
@@ -1,5 +1,18 @@
 package ar.edu.unju.edm.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.edm.model.Usuario;
+
+@Service
 public interface IUsuarioService {
+	
+	public void guardarUsuario(Usuario usuario);
+	public void eliminarUsuario(Integer id) throws Exception;
+	public void modificarUsuario(Usuario usuario);
+	public List<Usuario> listarUsuarios(); 
+	public Usuario buscarUsuario(Integer id) throws Exception;
 
 }
