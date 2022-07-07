a1ed99aa3cf21caa2ecd7f771c03a87ef4ec5fef
Leonel19Hub <leonel19coraite@gmail.com>
2022-07-06 22:53:43 -0300

    ultima modificacion

--- src/main/java/ar/edu/unju/edm/controller/PrincipalController.java
+++ src/main/java/ar/edu/unju/edm/controller/PrincipalController.java
@@ -1,15 +1,26 @@
 package ar.edu.unju.edm.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import ar.edu.unju.edm.service.IPeliculaService;
 
 @Controller
 public class PrincipalController {
-	 @GetMapping({"/","index","home"})
-     public String  getIndex() {
-		 
-  	   return "/index";
-  	   }
+	@Autowired
+    IPeliculaService peliculaService;
+	
+	@GetMapping({"/home", "/inicio", "/index", "/"})
+    public ModelAndView inicio(Model model){
+		ModelAndView modelView = new ModelAndView("index");
+		modelView.addObject("mostrarPeliculas", peliculaService.listarPeliculas());
+		modelView.addObject("mostrarSlider", peliculaService.peliculaSlider());
+		modelView.addObject("band", true);
+        return modelView;
+    }
 	 @GetMapping({"/","login","ingreso"})
      public String  getLogin() {
 		 
